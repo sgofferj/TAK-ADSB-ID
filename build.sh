56b54fd47f842a1c0f2b444563ac90e624934f8e
sgofferj <stefan@gofferje.net>
2023-02-16 15:00:33 +0200

    added JSON creation to build.sh and release cycle

--- build.sh
+++ build.sh
@@ -11,6 +11,16 @@ for DIR in ???; do
   egrep -v "^#" ${DIR}/SPC.txt >> tmp.txt
 done
 
+cat tmp.txt | sed "s/\"//g" | \
+jq -Rsn '
+  {"aircraft":
+    [inputs
+     | . / "\n"
+     | (.[] | select(length > 0) | . / ",") as $input
+     | {"hexid": $input[0], "cot": $input[1], "reg": $input[2], "type": $input[3], "operator": $input[4]}]}
+' >${RELEASEDATE}.json
+
+
 echo '#TAK ADSB IDs' > ${RELEASEDATE}.txt
 echo '#Copyright 2021 Stefan Gofferje' >> ${RELEASEDATE}.txt
 echo '#License: CC-BY-SA 4.0' >> ${RELEASEDATE}.txt
@@ -18,5 +28,5 @@ echo '#Format:' >> ${RELEASEDATE}.txt
 echo '#"hexid","SIDC","registration","type","operator"' >> ${RELEASEDATE}.txt
 echo >> ${RELEASEDATE}.txt
 cat tmp.txt | sort | egrep -v "^$" >> ${RELEASEDATE}.txt
-zip ${RELEASEDATE}.zip ${RELEASEDATE}.txt
-rm tmp.txt ${RELEASEDATE}.txt
+zip ${RELEASEDATE}.zip ${RELEASEDATE}.txt ${RELEASEDATE}.json
+rm tmp.txt ${RELEASEDATE}.txt ${RELEASEDATE}.json
